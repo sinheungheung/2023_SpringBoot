23f40c0fdbb1c7bd226910af81b96090bf2dc262
Heung <s2120@e-mirim.hs.kr>
2023-06-01 09:16:00 +0900

    서버에 이미지 올리기

---
+++ firstBoot/src/main/java/kr/hs/study/firstBoot/FirstBootApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.study.firstBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBootApplication.class, args);
+	}
+
+}
